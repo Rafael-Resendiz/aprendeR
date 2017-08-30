0d7f5c6a4f8f3f4e7cb32c1662bfd24197783a80
Rafael Resendiz Ramirez <rafaresend@gmail.com>
2017-08-29 20:01:02 -0700

    Add files via upload

---
+++ aprendeR/Consideraciones/initLesson.R
@@ -0,0 +1,11 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+      .get_course_path <- function(){
+      tryCatch(aprendeR:::aprendeR_courses_dir(),
+         error = function(c) {file.path(find.package("aprendeR"),"Courses")}
+          )
+      }
+
+      mis_datos <- read.csv(file.path(.get_course_path(), "aprendeR", "Consideraciones", "matriz1b.csv"))
