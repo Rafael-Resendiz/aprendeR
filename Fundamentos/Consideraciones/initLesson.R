465b327d7f9c1c7eb10eba7bc3ac442956b381f4
Rafael Resendiz Ramirez <rafaresend@gmail.com>
2017-10-09 15:36:53 -0700

    Update initLesson.R

--- Fundamentos/Consideraciones/initLesson.R
+++ Fundamentos/Consideraciones/initLesson.R
@@ -2,10 +2,10 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
-      .get_course_path <- function(){
-      tryCatch(aprendeR:::Fundamentos_courses_dir(),
-         error = function(c) {file.path(find.package("Consideraciones"),"Courses")}
-          )
-      }
+#      .get_course_path <- function(){
+#      tryCatch(aprendeR:::Fundamentos_courses_dir(),
+#         error = function(c) {file.path(find.package("Consideraciones"),"Courses")}
+#          )
+#      }
 
-      mis_datos <- read.csv(file.path(.get_course_path(), "Fundamentos", "Consideraciones", "matriz1b.csv"))
+#     mis_datos <- read.csv(file.path(.get_course_path(), "Fundamentos", "Consideraciones", "matriz1b.csv"))
