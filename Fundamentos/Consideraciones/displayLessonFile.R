857e5a23649457e6fb2fdf2adfe2012604cf17b5
Rafael Resendiz Ramirez <rafaresend@gmail.com>
2017-10-10 17:57:33 -0700

    Update displayLessonFile.R

--- Fundamentos/Consideraciones/displayLessonFile.R
+++ Fundamentos/Consideraciones/displayLessonFile.R
@@ -1,2 +1,8 @@
 # Este archivo debe desplegar el archivo lesson.yaml de la lección de consideraciones.
-display_swirl_file("lesson.yaml", "Fundamentos", "Consideraciones")
+# display_swirl_file("lesson.yaml", "Fundamentos", "Consideraciones")
+
+
+file <- system.file("lesson.yaml", package="Consideraciones")
+cat("Displaying: ", file, ":\n", sep="")
+displayCode(file)
+
